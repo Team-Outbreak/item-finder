3baa9d042b0a840af659805b048dd8b8ca8f7b2b
sbryant-developer <stephenbryant11@gmail.com>
2017-10-08 12:21:26 -0400

    #9: Add caching to allow the app to work offline
    All search results now include a two hour client-side cache. This should help a lot with searches inside a low-service store.
    Confirmed that after disconnecting the server, any pages I previously navigated to were still accessible. Note that hitting 'refresh' in a browser resets the cache for that page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+
+    # Cache for the next 2 hours (enough time to get the results only once in a store and not need to fetch them again)
+    expires_in 2.hours, :public => true
+
     @item = nil
     if params[:search]
       @item = Item.search(params[:search])
