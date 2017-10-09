cbf875dec53e9aa433e6124e0b0b2ffb348d7285
sbryant-developer <stephenbryant11@gmail.com>
2017-10-09 17:59:41 -0400

    #19 and #18: Allow users to request new items & association changes.
    Updated the google form integration to use embedded forms so that the user never leaves out site.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/more_info'
+  get 'welcome/suggestion_form'
+  get 'welcome/feedback_form'
 
   resources :items do
     resources :associations
